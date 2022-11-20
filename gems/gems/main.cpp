2e87a475463ecae1b6e58edd14f2fcd32431db96
notmk69 <97252132+notmk69@users.noreply.github.com>
2022-11-20 21:06:05 +0500

    scoreboard made

--- gems/gems/main.cpp
+++ gems/gems/main.cpp
@@ -96,7 +96,6 @@ int main()
     int flag = 1;
     int game = 0;
     
-   
     
 
     
@@ -169,10 +168,15 @@ int main()
      }
    if (game == 1)
     {
+       //time count start;
+        Clock gametime;
+        Time gametimedisplay;
 
+        //window:
         RenderWindow gamewindow(VideoMode(1202, 600), "GEMS GALAXY");
         gamewindow.setVerticalSyncEnabled(true);
-        //newgame:
+        //newgame: 
+        
         //background:
         Texture background;
         Image bg;
@@ -191,7 +195,7 @@ int main()
         RectangleShape grid(Vector2f(500, 500));
         const Texture* gridtexture = &gridtexture1;
         grid.setTexture(gridtexture);
-        grid.setPosition(100, 80);
+        grid.setPosition(75, 80);
         
         //buttons:
         Texture pause1;
@@ -201,8 +205,8 @@ int main()
         RectangleShape pauseshape(Vector2f(120,50));
         const Texture* pause = &pause1;
         pauseshape.setTexture(pause);
-        pauseshape.move(6, 0);
-
+        pauseshape.move(1000, 0);
+        //
         Texture crossbutton1;
         Image cross;
         cross.loadFromFile("cross2.png");
@@ -212,12 +216,66 @@ int main()
         cross2.setTexture(crossbutton);
         cross2.move(1155, 0);
 
+        //time bar:
+        Texture timebartexture1;
+        Image timebar100;
+        timebar100.loadFromFile("100%.png");
+        timebartexture1.loadFromImage(timebar100);
+        RectangleShape timebar(Vector2f(300, 30));
+        const Texture* timebartexture = &timebartexture1;
+        timebar.setTexture(timebartexture);
+        timebar.move(180, 40);
 
+        Image timebar90;
+        timebar90.loadFromFile("90%.png");
 
+        Image timebar80;
+        timebar80.loadFromFile("80%.png");
 
+        Image timebar70;
+        timebar70.loadFromFile("70%.png");
 
+        Image timebar60;
+        timebar60.loadFromFile("60%.png");
+
+        Image timebar50;
+        timebar50.loadFromFile("50%.png");
+
+        Image timebar40;
+        timebar40.loadFromFile("40%.png");
+
+        Image timebar30;
+        timebar30.loadFromFile("30%.png");
+        
+        Image timebar20;
+        timebar20.loadFromFile("20%.png");
+
+        Image timebar10;
+        timebar10.loadFromFile("10%.png");
+
+        //scoreboard:
+        Texture scoreboardtexture1;
+        Image scoreboardimage;
+        scoreboardimage.loadFromFile("score.png");
+        scoreboardtexture1.loadFromImage(scoreboardimage);
+        RectangleShape scoreboard(Vector2f(450, 500));
+        const Texture* scoreboardtexture = &scoreboardtexture1;
+        scoreboard.setTexture(scoreboardtexture);
+        scoreboard.move(650, -50);
+
+        Text score;
+        score.setFont(headingfont);
+        score.setString("SCORE");
+        score.Bold;
+        score.move(770, 120);
+        score.setCharacterSize(60);
+        score.setFillColor(Color::Black);
+        score.setOutlineColor(Color::Green);
+        score.setOutlineThickness(5);
+      
         while (gamewindow.isOpen())
         {
+            gametimedisplay = gametime.getElapsedTime();
             sf::Event closing;
             while (gamewindow.pollEvent(closing))
             {
@@ -228,28 +286,56 @@ int main()
                         gamewindow.close();
                     }
                 }
-                sf::Event closing;
-               
-                
-                   ;
-                    
 
-
-                    gamewindow.clear();
-                    gamewindow.draw(newgamebg);
-                    gamewindow.draw(grid);
-                    gamewindow.draw(cross2);
-                    gamewindow.draw(pauseshape);
-                    gamewindow.display();
-
-
-
-
-
-
-
-                
+                gamewindow.clear();
+                gamewindow.draw(newgamebg);
+                gamewindow.draw(grid);
+                gamewindow.draw(cross2);
+                gamewindow.draw(pauseshape);                                                                                    //change later!!!!!!!!!!!!!!!!!!!!
+                gamewindow.draw(timebar);
+                gamewindow.draw(scoreboard);
+                gamewindow.draw(score);
+                gamewindow.display();
             }
+            //timebar updates:
+            if (gametimedisplay.asSeconds() < 21 && gametimedisplay.asSeconds() > 10)
+            {
+                timebartexture1.loadFromImage(timebar90);
+            }
+            else if (gametimedisplay.asSeconds() < 31 && gametimedisplay.asSeconds() > 20)
+            {
+                timebartexture1.loadFromImage(timebar80);
+            }
+            else if (gametimedisplay.asSeconds() < 41 && gametimedisplay.asSeconds() > 30)
+            {
+                timebartexture1.loadFromImage(timebar70);
+            }
+            else if (gametimedisplay.asSeconds() < 51 && gametimedisplay.asSeconds() > 40)
+            {
+                timebartexture1.loadFromImage(timebar60);
+            }
+            else if (gametimedisplay.asSeconds() < 61 && gametimedisplay.asSeconds() > 50)
+            {
+                timebartexture1.loadFromImage(timebar50);
+            }
+            else if (gametimedisplay.asSeconds() < 71 && gametimedisplay.asSeconds() > 60)
+            {
+                timebartexture1.loadFromImage(timebar40);
+            }
+            else if (gametimedisplay.asSeconds() < 81 && gametimedisplay.asSeconds() > 70)
+            {
+                timebartexture1.loadFromImage(timebar30);
+            }
+            else if (gametimedisplay.asSeconds() < 91 && gametimedisplay.asSeconds() > 80)
+            {
+                timebartexture1.loadFromImage(timebar20);
+            }
+            else if (gametimedisplay.asSeconds() < 101 && gametimedisplay.asSeconds() > 90)
+            {
+                timebartexture1.loadFromImage(timebar10);
+            }
+            else if (gametimedisplay.asSeconds()>101) cout << "game over";                                                      //change later!!!!!!!!!!!!!!!!!!!
+            
         }
    }
     return 0;
